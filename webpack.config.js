02d9c6fc56b97f90a4c1ad48a81aaf499acbe830
sinbiotik <111578346+sinbiotik@users.noreply.github.com>
2023-03-05 20:51:53 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,51 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
 
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash]${ext}`
+const optimization = () => {	
+	const config = {
+		splitChunks: {
+			chunks: 'all'
+		}
+	}
+	if (isProd) {
+		config.minimizer = [
+			new CssMinimizerWebpackPlugin(),
+			new TerserWebpackPlugin()
+		]
+	}
+	return config
+}
 
 module.exports = {
-  // context: path.resolve(__dirname, 'src'),    ?????
   mode: "development",
   entry: {
     main: './src/index.tsx',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash].js',
+    filename: filename('.js'),
     clean: true,              
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 460000,
+    maxAssetSize: 460000
+  },
+  optimization: optimization(),
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
     }),
-    // new CopyWebpackPlugin({ 
-    //   patterns : [
-    //     {
-    //       from : path.resolve(__dirname, 'src/img/hacker-news-logo.png'),
-    //       to: path.resolve(__dirname, 'dist') 
-    //     }, 
-    //   ], 
-    // }),
+    new MiniCssExtractPlugin({
+      filename: filename('.css'),
+    })
   ],
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
@@ -39,15 +59,16 @@ module.exports = {
     rules: [
       {
 				test: /\.css$/,
-				use: ["style-loader", "css-loader"]
+				use: [{
+            loader: MiniCssExtractPlugin.loader,
+          },					
+          'css-loader',]
 			},
-
       {
         test: /\.([cm]?ts|tsx)$/,
-        exclude: /node_modules/,   // ??????
+        exclude: /node_modules/,
         loader: "ts-loader",
       },
-
       {
 				test: /\.(png|jpg|svg|gif)$/,
         type: 'asset/resource'
@@ -55,7 +76,8 @@ module.exports = {
     ],
   },
   devServer: {
-    port: 3000
+    port: 3000,
+    hot: isDev
   },
 };
 
@@ -65,14 +87,15 @@ module.exports = {
 
 // npm install --save-dev webpack
 // npm install --save-dev webpack-cli
-
-// npm install -D webpack-dev-server           
-
+// npm install -D webpack-dev-server
 // npm install --save-dev html-webpack-plugin
-
 // npm install --save-dev style-loader
 // npm install --save-dev css-loader
-
 // npm install ts-loader --save-dev
+// npm install css-minimizer-webpack-plugin --save-dev
+// npm install --save-dev cross-env
+// npm install terser-webpack-plugin --save-dev
+// npm i --save lodash
 
-// npm i -D copy-webpack-plugin
+
+
